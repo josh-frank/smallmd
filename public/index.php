1df5ae85383158956d5e1fef77fb9d18433c9367
Josh Frank <josh_frank@icloud.com>
2026-04-30 14:45:52 -0400

    adding hashes to csp

--- public/index.php
+++ public/index.php
@@ -9,7 +9,16 @@ use SmallMD\Router;
 use SmallMD\Config;
 
 $nonce = base64_encode(random_bytes(16));
-header("Content-Security-Policy: default-src 'self'; style-src 'self' 'nonce-{$nonce}'; script-src 'nonce-{$nonce}'; img-src 'self' data:; font-src 'self'; frame-ancestors 'none';");
+header("Content-Security-Policy: " . implode('; ', [
+    "default-src 'self'",
+    "style-src 'self' 'nonce-{$nonce}' https://*.googleapis.com 'unsafe-hashes' 'sha256-a6mcqZ043xibzvNjj7LmbkIhZ0e0BjYZ/2qih6Mqw1o=' 'sha256-Vmf35/seiSHA++jhCBJCep+j1l498+1v8RnVbox53XU='",
+    "style-src-attr 'unsafe-hashes' 'sha256-a6mcqZ043xibzvNjj7LmbkIhZ0e0BjYZ/2qih6Mqw1o=' 'sha256-Vmf35/seiSHA++jhCBJCep+j1l498+1v8RnVbox53XU='",
+    "script-src 'nonce-{$nonce}' 'unsafe-hashes' 'sha256-BylnATObMDEcEC05wmP4SZ95f/7Zsp0ok0W7UYkBgKQ='",
+    "script-src-attr 'unsafe-hashes' 'sha256-BylnATObMDEcEC05wmP4SZ95f/7Zsp0ok0W7UYkBgKQ='",
+    "img-src 'self' data: https://*.linodeobjects.com",
+    "font-src 'self' https://*.gstatic.com",
+    "frame-ancestors 'none'",
+]));
 header('X-Frame-Options: DENY');
 header('X-Content-Type-Options: nosniff');
 header('Referrer-Policy: strict-origin-when-cross-origin');
